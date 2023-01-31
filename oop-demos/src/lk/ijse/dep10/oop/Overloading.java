28ae675bb174aea151d8258b5d939b99ba9dc1ea
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-01-31 19:47:05 +0530

    Add leftovers

--- oop-demos/src/lk/ijse/dep10/oop/Overloading.java
+++ oop-demos/src/lk/ijse/dep10/oop/Overloading.java
@@ -1,2 +1,15 @@
-package lk.ijse.dep10.oop;public class Overloading {
+package lk.ijse.dep10.oop;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Overloading {
+
+    public static void printSomething(Stack<String> y){  // m1
+
+    }
+
+    public static void printSomething(List<String> x){  // m2
+
+    }
 }
